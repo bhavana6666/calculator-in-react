e3fad2f3530686ad6ee409679074a7a6c688ef91
Your Name <you@example.com>
2023-11-02 16:29:06 +0530

    calculator in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
 function App() {
+  const [result, setResult] = useState("");
+  const handleClick = (e) => {
+    setResult(result+(e.target.value))
+  }
+  const clear = () => {
+    setResult("");
+  }
+  const calculate = () => {
+    setResult(eval(result))
+  }
+  const del=()=>{
+    setResult(result.slice(0,-1))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CALCULATOR</h1>
+      <form>
+        <input type="text" value={result}></input>
+      </form>
+      <div className='keys'>
+        <button value="clear" onClick={clear}>Clear</button>
+        <button value="7" onClick={handleClick}>7</button>
+        <button value="8" onClick={handleClick}>8</button>
+        <button value="9" onClick={handleClick}>9</button>
+        <button value="4" onClick={handleClick}>4</button>
+        <button value="5" onClick={handleClick}>5</button>
+        <button value="6" onClick={handleClick}>6</button>
+        <button value="-" onClick={handleClick}>-</button>
+        <button value="1" onClick={handleClick}>1</button>
+        <button value="2" onClick={handleClick}>2</button>
+        <button value="3" onClick={handleClick}>3</button>
+        <button value="+" onClick={handleClick}>+</button>
+        <button value="0" onClick={handleClick}>0</button>
+        <button value="del" onClick={del}>DEL</button>
+        <button value="*" onClick={handleClick}>*</button>
+        <button className="equal" onClick={calculate}>=</button>
+      </div>
     </div>
   );
 }
-
 export default App;
